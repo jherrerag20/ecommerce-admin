b581488ab37eb3396fa125ecfc8f2459f17897f5
Lomosaurio123 <mhernandezl1605@alumno.ipn.mx>
2023-11-09 01:09:55 -0600

    modificando las graficas del dashboard

--- actions/get-graph-revenue.ts
+++ actions/get-graph-revenue.ts
@@ -25,11 +25,7 @@ export const getGraphRevenue = async (storeId: string): Promise<GraphData[]> => 
   // Grouping the orders by month and summing the revenue
   for (const order of paidOrders) {
     const month = order.createdAt.getMonth(); // 0 for Jan, 1 for Feb, ...
-    let revenueForOrder = 0;
-
-    for (const item of order.orderItems) {
-      revenueForOrder += item.product.price.toNumber();
-    }
+    let revenueForOrder = order.totalPrice.toNumber();
 
     // Adding the revenue for this order to the respective month
     monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder;
