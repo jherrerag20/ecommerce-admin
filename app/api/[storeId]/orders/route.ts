e84ecc80c151522f12612b459a0f6c38639fee55
Lomosaurio123 <mhernandezl1605@alumno.ipn.mx>
2023-11-09 00:41:16 -0600

    haciendo pruebas

--- app/api/[storeId]/orders/route.ts
+++ app/api/[storeId]/orders/route.ts
@@ -3,35 +3,30 @@ import { NextResponse } from 'next/server';
 import prismadb from '@/lib/prismadb';
 
 export async function GET(
-    req: Request,
-    { params }: { params: { storeId: string } }
-  ) {
-    try {
+  req: Request,
+  { params }: { params: { storeId: string; phone: string } }
+) {
+  try {
+    const { storeId, phone } = params;
 
-      const body = await req.json();
-
-      const { phone } = body
-
-      if (!phone) {
-        return new NextResponse("Phone is required", { status: 400 });
-      }
-
-      if (!params.storeId) {
-        return new NextResponse("Store id is required", { status: 400 });
-      }
-      
-    
-      const orders = await prismadb.order.findMany({
-        where: {
-          // phone: phone,
-          storeId: params.storeId
-        }
-      });
-        
-      return NextResponse.json(orders);
-    } catch (error) {
-
-      console.log('[ORDERS_GET]', error);
-      return new NextResponse("Internal error", { status: 500 });
+    if (!phone) {
+      return new NextResponse("Phone is required", { status: 400 });
     }
-  };
+
+    if (!storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const orders = await prismadb.order.findMany({
+      where: {
+        phone: phone,
+        storeId: storeId,
+      },
+    });
+
+    return NextResponse.json(orders);
+  } catch (error) {
+    console.log('[ORDERS_GET]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
