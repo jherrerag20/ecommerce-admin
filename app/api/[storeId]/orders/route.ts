dcc8bb31b49f90094bf3d585511032068978beea
Lomosaurio123 <mhernandezl1605@alumno.ipn.mx>
2023-11-09 00:37:49 -0600

    pruebas

--- app/api/[storeId]/orders/route.ts
+++ app/api/[storeId]/orders/route.ts
@@ -23,7 +23,7 @@ export async function GET(
     
       const orders = await prismadb.order.findMany({
         where: {
-          phone: phone,
+          // phone: phone,
           storeId: params.storeId
         }
       });
