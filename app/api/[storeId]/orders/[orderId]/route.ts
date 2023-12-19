8518872d74eae449308347f4491ed1cfe8f0f880
Mauricio Gael Hernández Lom <85517698+Lomosaurio123@users.noreply.github.com>
2023-12-19 15:03:15 -0600

    Update route.ts del delete

--- app/api/[storeId]/orders/[orderId]/route.ts
+++ app/api/[storeId]/orders/[orderId]/route.ts
@@ -3,55 +3,81 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  };
-  
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function OPTIONS() {
-    return NextResponse.json({}, { headers: corsHeaders });
+  return NextResponse.json({}, { headers: corsHeaders });
 }
 
 export async function DELETE(
-    _req : Request,
-    { params } : { params: { storeId : string, orderId : string } }
+  _req: Request,
+  { params }: { params: { storeId: string; orderId: string } }
 ) {
+  try {
+    const { userId } = auth();
 
-    try {
-
-        const { userId }= auth();
-
-
-        if( !userId ) {
-            return new NextResponse( "Unauthenticated", { status : 401 } );
-        }
-
-        if( !params.orderId ) {
-            return new NextResponse( "Order id is required", { status : 400 } );
-        }
-
-        const storeByUserId = await prismadb.store.findFirst({
-            where: {
-              id: params.storeId,
-              userId,
-            }
-          });
-
-        if (!storeByUserId) {
-            return new NextResponse("Unauthorized", { status: 405 });
-        }
-
-        const order = await prismadb.order.deleteMany({
-            where : {
-                id : params.orderId,
-            },
-        });
-
-        return NextResponse.json( order, { headers: corsHeaders } ); 
-        
-    } catch (error) {
-        console.log('[ORDER_DELETE]', error);
-        return new NextResponse("Internal Error", { status : 500 });
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
     }
-    
-};
+
+    if (!params.orderId) {
+      return new NextResponse("Order id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 405 });
+    }
+
+    // Retrieve order details including order items
+    const order = await prismadb.order.findUnique({
+      where: {
+        id: params.orderId,
+      },
+      include: {
+        orderItems: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
+
+    if (!order) {
+      return new NextResponse("Order not found", { status: 404 });
+    }
+
+    // Update product amounts for each order item
+    for (const orderItem of order.orderItems) {
+      await prismadb.product.update({
+        where: {
+          id: orderItem.product.id,
+        },
+        data: {
+          amount: orderItem.product.amount + orderItem.productsAmount,
+        },
+      });
+    }
+
+    // Delete the order
+    await prismadb.order.delete({
+      where: {
+        id: params.orderId,
+      },
+    });
+
+    return NextResponse.json({ message: "Order deleted successfully" }, { headers: corsHeaders });
+  } catch (error) {
+    console.error('[ORDER_DELETE]', error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
